4f926738e913a24abb6c1c3d2916f56c2b4db30d
Sean Stamm <sean.stamm5@gmail.com>
2018-02-23 15:12:29 +0800

    Add RandomizedQueue implementation

--- StacksQueuesSorts/RandomizedQueue.java
+++ StacksQueuesSorts/RandomizedQueue.java
@@ -1,10 +1,19 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] q;
     private int n;
+    private int first;
+    private int last;
 
     public RandomizedQueue() {
         q = (Item[]) new Object[2];
         n = 0;
+        first = 0;
+        last = 0;
     }
 
     public boolean isEmpty() {
@@ -16,22 +25,80 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
     
     public void enqueue(Item item) {
-
+        if (item == null) throw new IllegalArgumentException();
+        q[last++] = item;
+        if (last == q.length) last = 0;
+        n++;
+        if (n == q.length) resize(q.length * 2);
     }
 
     public Item dequeue() {
-
-    }                   // remove and return a random item
+        if (isEmpty()) throw new NoSuchElementException();
+        int r = random();
+        Item item = q[r];
+        q[r] = q[first];
+        q[first++] = null;
+        if (first == q.length) first = 0;
+        n--;
+        if (n > 0 && n == q.length / 4) resize(q.length/2);
+        return item;
+    }
 
     public Item sample() {
-
-    }                    // return a random item (but do not remove it)
+        if (isEmpty()) throw new NoSuchElementException();
+        int r = random();
+        Item item = q[r];
+        return item;
+    }
 
     public Iterator<Item> iterator() {
+        return new ArrayIterator();
+    }
 
-    }        // return an independent iterator over items in random order
+    private class ArrayIterator implements Iterator<Item> {
+        private int i = 0;
+        private Item[] a;
+        public ArrayIterator() {
+            a = (Item[]) new Object[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = q[(first + i) % q.length];
+            }
+            StdRandom.shuffle(a);
+        }
+        public boolean hasNext()  { return i < n;                              }
+        public void remove()      { throw new UnsupportedOperationException(); }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = a[i];
+            i++;
+            return item;
+        }
+    }
+
+    private void resize(int size) {
+        Item[] temp = (Item[]) new Object[size];
+        for (int i = 0; i < q.length; i++) {
+            temp[i] = q[i];
+        }
+        first = 0;
+        last = 0;
+        q = temp;
+    }
+
+    private int random() {
+        int r;
+        if (last < first) {
+            r = StdRandom.uniform(first, last + q.length);
+            r = r % q.length;
+        } else {
+            r = StdRandom.uniform(first, last);
+        }
+
+        return r;
+    }
     
     public static void main(String[] args) {
-
-    }  // unit testing (optional)
+        
+    }
 }
